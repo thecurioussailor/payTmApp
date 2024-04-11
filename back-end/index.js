6d98980413e5de9d29ab0b79273a941963aa4881
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-04-11 11:58:54 +0530

    checking gitignore working

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const cors = require("cors");
 const mainRouter = require("./routes/index");
-
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
