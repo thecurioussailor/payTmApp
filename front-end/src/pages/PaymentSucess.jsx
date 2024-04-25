c9bf831b5d8fe27ee07cfd6a7b59acfddf239323
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-04-25 23:16:25 +0530

    added redirects

--- front-end/src/pages/PaymentSucess.jsx
+++ front-end/src/pages/PaymentSucess.jsx
@@ -41,7 +41,6 @@ export default function PaymentSucess() {
                     </div>
                     <button onClick={handleDashboard} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Dashboard
-        
                     </button>
                 </div>
                 </div>
